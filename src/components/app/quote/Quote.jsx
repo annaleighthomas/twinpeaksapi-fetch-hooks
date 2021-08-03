853740dfdecbc31a8c50365368ada753bbaa73ec
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-03 12:33:43 -0400

    quote component

---
+++ src/components/app/quote/Quote.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Quote = ({ quote }) => (
+  <p>{quote}</p>
+);
+
+Quote.propTypes = {
+  quote: PropTypes.string.isRequired
+};
+
+export default Quote;
