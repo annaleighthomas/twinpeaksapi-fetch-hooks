fb1263a21c3e50e1e7a7e97107b19c0e66ec9af5
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-03 12:51:04 -0400

    twin peaks container

---
+++ src/containers/TwinPeaksQuotes.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Load from '../components/app/quote/Load';
+import Quote from '../components/app/quote/Quote';
+import { fetchQuote } from '../services/twinPeaksApi';
+
+const TwinPeaksQuotes = () => {
+  const [quote, setQuote] = useState('Fetch Quote');
+
+  const handleClick = async () => {
+    const res = await fetchQuote();
+    const quote = JSON.stringify(res);
+
+    setQuote(quote);
+  };
+
+  return (
+    <>
+      <Quote quote={quote} />
+      <Load onClick={handleClick} />
+    </>
+  );
+
+};
+
+export default TwinPeaksQuotes;
